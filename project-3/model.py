809d4b6d11a25d7c2881d59d3e5f764bd482d5ed
YoshiyukiKono <yoshiyuki.kono@gmail.com>
2017-12-04 07:26:52 +0900

    Successfull Result

--- project-3/model.py
+++ project-3/model.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 import sklearn
+import tensorflow as tf
 
 # import the necessary Keras packages
 from keras import backend as K
@@ -16,80 +17,31 @@ from keras.utils import np_utils
 from keras.optimizers import SGD, RMSprop, Adam
 
 import keras
-print(keras.__version__)
+#print(keras.__version__)
 
-DATADIR = "./data/"
-DIR_IMG = './data/IMG/'
 DIR_DATA = "./data/"
-
-#define the convnet 
-class LeNet:
-	@staticmethod
-	def build(input_shape, classes):
-		print(input_shape)
-		model = Sequential()
-		# CONV => RELU => POOL
-	
-		model.add(Conv2D(20, #kernel_size=5, #padding="same",
-			input_shape=input_shape, nb_row=160, nb_col=320))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-		# CONV => RELU => POOL
-		model.add(Conv2D(50, kernel_size=5, padding="same"))
-		model.add(Activation("relu"))
-		model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-		# Flatten => RELU layers
-		model.add(Flatten())
-		model.add(Dense(500))
-		model.add(Activation("relu"))
- 
-		# a softmax classifier
-		if classes != 1:
-			model.add(Dense(classes))
-			model.add(Activation("softmax"))
-		if classes == 1:
-			model.add(Flatten(input_shape=input_shape))
-			model.add(Dense(1))
-		return model
+DRIVING_LOG = './data/driving_log.csv'
 
 # network and training
-NB_EPOCH = 3#8#6#4# 2#10# 4 #7#20
+NB_EPOCH = 8#3#8#6#4# 2#10# 4 #7#20
 BATCH_SIZE = 128
 VERBOSE = 1
-OPTIMIZER = Adam()
 VALIDATION_SPLIT=0.2
 
-IMG_ROWS, IMG_COLS = 160, 320 #28, 28 # input image dimensions
-NB_CLASSES = 1#10  # number of outputs = number of digits
-INPUT_SHAPE = (160, 320, 3)#(1, IMG_ROWS, IMG_COLS)	
+INPUT_SHAPE = (160, 320, 3)	
 
-IMG_DIM = 3
-
-def getLog():
+def get_logs(file_path):
+	"""get the lines from the given file path as csv file"""
 	lines = []
-	with open('./data/driving_log.csv') as csvfile:
+	with open(file_path) as csvfile:
 		reader = csv.reader(csvfile)
 		fieldnames = next(reader)
 		for line in reader:			
 			lines.append(line)
 	return lines
 
-def load(lines):  
-	images = []
-	measurements = []
-	for line in lines:
-		source_path = line[0]
-		filename = source_path.split('/')[-1]
-		current_path = DIR_IMG + filename
-		image = cv2.imread(current_path)
-		images.append(image)	
-		measurement = float(line[3])
-		measurements.append(measurement)	
-	x_train = np.array(images)
-	y_train = np.array(measurements)
-	return x_train, y_train
-
 def equalize_histogram(image):
+	"""apply histgram equalization to the given image"""
 	copied_image = image.copy()
 	copied_image[:,:,0] = cv2.equalizeHist(image[:,:,0])
 	copied_image[:,:,1] = cv2.equalizeHist(image[:,:,1])
@@ -97,60 +49,13 @@ def equalize_histogram(image):
 	return copied_image
 
 def process_image(image):
-	if IMG_DIM == 1:
-		print("RGB",np.array(image).shape)
-		processed_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-		print("GRAY",np.array(processed_image).shape)
-	else:
-		processed_image = equalize_histogram(image)
-	#image_copy = gray_image.copy()	
-	#image_copy[:,:,0] = cv2.equalizeHist(gray_image_copy[:,:,0])	
-	#return image_copy
-	return processed_image#gray_image
-
-def loadWithFlipImage(lines):  
-	images = []
-	measurements = []
-	for line in lines:
-		#source_path = line[0]
-		#filename = source_path.split('/')[-1]
-		#current_path = DIR_IMG + filename
-		#image = cv2.imread(current_path)
-		image_org = cv2.imread(DIR_DATA + line[0].strip())
-		img_left_org = cv2.imread(DIR_DATA + line[1].strip())
-		img_right_org = cv2.imread(DIR_DATA + line[2].strip())
-		
-		image = process_image(image_org)
-		img_left = process_image(img_left_org)
-		img_right = process_image(img_right_org)		
-
-		images.extend([image, img_left, img_right])
-		#images.append(image)
-		#images.append(img_left)
-		#images.append(img_right)
-		
-		measurement = float(line[3])
-		correction = 0.2 # this is a parameter to tune
-		steering_left = measurement + correction
-		steering_right = measurement - correction
-		
-		#measurements.append(measurement)
-		#measurements.append(steering_left)
-		#measurements.append(steering_right)
-		measurements.extend([measurement, steering_left, steering_right])
-		
-		image_flipped = np.fliplr(image)
-		measurement_flipped = -measurement
-		images.append(image_flipped)	
-		measurements.append(measurement_flipped)		
-	
-	x_train = np.array(images)
-	y_train = np.array(measurements)
-	return x_train, y_train
+	"""process an image"""
+	processed_image = equalize_histogram(image)
+	return processed_image
 
 
-
-def loadWithMultipleCameras(rows):  
+def get_data_pair(rows):
+	"""load data set from training logs"""
 	car_images = []
 	steering_angles = []
 	for row in rows:
@@ -161,15 +66,12 @@ def loadWithMultipleCameras(rows):
 		steering_left = steering_center + correction
 		steering_right = steering_center - correction			
 		
-		#img_center = process_image(np.asarray(cv2.imread(DIR_DATA + row[0].strip())))
-		#img_left = process_image(np.asarray(cv2.imread(DIR_DATA + row[1].strip())))
-		#img_right = process_image(np.asarray(cv2.imread(DIR_DATA + row[2].strip())))
-		
 		img_center = cv2.imread(DIR_DATA + row[0].strip())
 		img_left = cv2.imread(DIR_DATA + row[1].strip())
 		img_right = cv2.imread(DIR_DATA + row[2].strip())
 		
-		image = process_image(img_center)
+		#!!!!!!!!!!!!!image = process_image(img_center)
+		#img_center = process_image(img_center)
 		img_left = process_image(img_left)
 		img_right = process_image(img_right)		
     
@@ -195,36 +97,16 @@ def loadWithMultipleCameras(rows):
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers.convolutional import Convolution2D
+from keras.callbacks import EarlyStopping
 
-#from keras.layers.pooling...
-def build():
+def build_model(keep_prob=0.5):
+	"""build a model introduced by NVIDEA"""
 	model = Sequential()
-	model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160, 320, IMG_DIM)))
-	model.add(Lambda(lambda x: x / 255.0 -0.5))
-	#model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160, 320, 3)))
-	#model.add(Flatten())
-	#model.add(Flatten(input_shape=(160, 320, 3)))
-	
-	# CONV => RELU => POOL
-	model.add(Conv2D(32,3,3))
-	model.add(Activation("relu"))
-	model.add(Conv2D(32,3,3))
-	model.add(Activation("relu"))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	# CONV => RELU => POOL
-	model.add(Conv2D(64, 3,3))
-	model.add(Activation("relu"))
-	model.add(MaxPooling2D(pool_size=(2, 2)))
-	
-	# Flatten => RELU layers
-	model.add(Flatten())	
 
-	model.add(Dense(1))
-	return model
-
-def buildNVIDEA(keep_prob=0.5):
-	model = Sequential()
-	model.add(Lambda(lambda x: x / 255.0 -0.5,input_shape=(160, 320, IMG_DIM)))
+	model.add(Lambda(lambda x: x / 255.0 -0.5,input_shape=INPUT_SHAPE))
+		
+	Lambda(lambda x: tf.image.rgb_to_grayscale(x))
+	
 	model.add(Cropping2D(cropping=((70,25),(0,0))))
 	model.add(Convolution2D(24,5,5, subsample=(2,2), activation="relu"))
 	model.add(Convolution2D(36,5,5, subsample=(2,2), activation="relu"))
@@ -239,46 +121,10 @@ def buildNVIDEA(keep_prob=0.5):
 	model.add(Dense(1))
 	return model
 
-def buildForGenerator():
-	
-	ch, row, col = 3, 80, 320  # Trimmed image format
-	
-	model = Sequential()
-	model.add(Lambda(lambda x: x/127.5 - 1.,
-		input_shape=(ch, row, col),
-		output_shape=(ch, row, col)))	
-	#model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160, 320, 3)))
-	#model.add(Lambda(lambda x: x / 255.0 -0.5))
-	#model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160, 320, 3)))
-	#model.add(Flatten())
-	model.add(Flatten(input_shape=(160, 320, 3)))
-	model.add(Dense(1))
-	return model	
-	model = Sequential()
-	##model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160, 320, 3)))
-	#model.add(Lambda(lambda x: x / 255.0 -0.5))
-	model.add(Lambda(lambda x: x / 255.0 -0.5, input_shape=(160, 320, 3)))
-	#model.add(Flatten())
-	model.add(Flatten(input_shape=(160, 320, 3)))
-	model.add(Dense(1))
-	
-	return model
-
-"""
-model.add(MaxPooling2D())
-model.add(Flattern())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
-"""
-
-### plot the training and validation loss for each epoch
 import matplotlib.pyplot as plt
 
-def showHistory(history_object) :
-	### print the keys contained in the history object
-	print(history_object.history.keys())
-	
+def display_graph(history_object) :
+	"""plot the training and validation loss for each epoch"""	
 	plt.plot(history_object.history['loss'])
 	plt.plot(history_object.history['val_loss'])
 	plt.title('model mean squared error loss')
@@ -287,135 +133,26 @@ def showHistory(history_object) :
 	plt.legend(['training set', 'validation set'], loc='upper right')
 	plt.show()
 
-def generator(samples, batch_size=32):
-	num_samples = len(samples)
-	while 1: # Loop forever so the generator never terminates
-		#shuffle(samples)
-		for offset in range(0, num_samples, batch_size):
-			batch_samples = samples[offset:offset+batch_size]
-
-			car_images = []
-			steering_angles = []
-			for row in batch_samples:
-				steering_center = float(row[3])
-				# create adjusted steering measurements for the side camera images
-				correction = 0.2 # this is a parameter to tune
-				steering_left = steering_center + correction
-				steering_right = steering_center - correction
-				
-				img_center = cv2.imread(DIR_DATA + row[0].strip())
-				img_left = cv2.imread(DIR_DATA + row[1].strip())
-				img_right = cv2.imread(DIR_DATA + row[2].strip())
-				
-				img_center = process_image(img_center)
-				img_left = process_image(img_left)
-				img_right = process_image(img_right)				
-			    
-				# add images and angles to data set
-				car_images.extend([img_center, img_left, img_right])
-				steering_angles.extend([steering_center, steering_left, steering_right])
-			    
-				img_center_flipped = np.fliplr(img_center)
-				img_left_flipped = np.fliplr(img_left)
-				img_right_flipped = np.fliplr(img_right)
-				steering_center_flipped = -steering_center
-				steering_left_flipped = -steering_center - correction
-				steering_right_flipped = -steering_center + correction
-				car_images.extend([img_center_flipped])#, img_left_flipped, img_right_flipped])
-				steering_angles.extend([steering_center_flipped])#, steering_left_flipped, steering_right_flipped])
-			x_train = np.array(car_images)
-			y_train = np.array(steering_angles)
-			yield sklearn.utils.shuffle(x_train, y_train)
-		
-def generatorPre(samples, batch_size=32):
-    num_samples = len(samples)
-    while 1: # Loop forever so the generator never terminates
-        #shuffle(samples)
-        for offset in range(0, num_samples, batch_size):		
-            batch_samples = samples[offset:offset+batch_size]
-
-            images = []
-            angles = []
-            for batch_sample in batch_samples:
-                name = './IMG/'+batch_sample[0].split('/')[-1]
-                center_image = cv2.imread(name)
-                center_angle = float(batch_sample[3])
-                images.append(center_image)
-                angles.append(center_angle)
-
-            # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
-            yield sklearn.utils.shuffle(X_train, y_train)
-	    
-def visualize_model(model, file_name="model.png"):
-	from keras.utils.visualize_util import plot
-	plot(model, to_file=file_name, show_shapes=True)	
-	    
-##################################
-# Main Logic
-##################################
-samples = getLog()
-
-from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-
-
-#x_train, y_train = load(train_samples)
-#x_test, y_test = load(validation_samples)
-
-#x_train, y_train = loadWithFlipImage(train_samples)
-#x_test, y_test = loadWithFlipImage(validation_samples)
-
-"""
-x_train, y_train = loadWithMultipleCameras(train_samples)
-x_test, y_test = loadWithMultipleCameras(validation_samples)
-
-print("x_train.shape",x_train.shape)
-print("y_train.shape",y_train.shape)
-"""
-
-#model = build()
-model = buildNVIDEA(keep_prob=0.5)
-#model = buildForGenerator()
-#model.compile(loss='mse', optimizer='adam')
-#model.fit(x_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=NB_EPOCH)
-
-# initialize the optimizer and model
-#model = LeNet.build(input_shape=INPUT_SHAPE, classes=NB_CLASSES)
-model.compile(loss='mse', optimizer='adam')#"categorical_crossentropy", optimizer=OPTIMIZER,
-	#metrics=["accuracy"])
-
-USE_GENERATOR = False
-if USE_GENERATOR:
-	# compile and train the model using the generator function
-	train_generator = generator(train_samples, batch_size=BATCH_SIZE)#32)
-	validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)#32)
-	history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples),
-		                             validation_data=validation_generator, nb_val_samples=len(validation_samples),
-		                             nb_epoch=NB_EPOCH)
-else:
-	x_train, y_train = loadWithMultipleCameras(train_samples)
-	x_test, y_test = loadWithMultipleCameras(validation_samples)
+if __name__ == '__main__':
 	
-	print("x_train.shape",x_train.shape)
-	print("y_train.shape",y_train.shape)	
+	samples = get_logs(DRIVING_LOG)
+
+	from sklearn.model_selection import train_test_split
+	train_samples, test_samples = train_test_split(samples, test_size=0.2)
+
+	model = build_model(keep_prob=0.5)
+	model.compile(loss='mse', optimizer='adam')
+
+	x_train, y_train = get_data_pair(train_samples)
+	x_test, y_test = get_data_pair(test_samples)
+		
 	history_object = model.fit(x_train, y_train, 
-		        batch_size=BATCH_SIZE, nb_epoch=NB_EPOCH, #epochs=NB_EPOCH, 
-		        verbose=VERBOSE, validation_split=VALIDATION_SPLIT, shuffle=True)	
-"""
-ValueError: output of generator should be a tuple (x, y, sample_weight) or (x, y). Found: None
-"""
+		        batch_size=BATCH_SIZE, nb_epoch=NB_EPOCH, 
+		        verbose=VERBOSE, validation_split=VALIDATION_SPLIT, shuffle=True,callbacks=[EarlyStopping()])	
 
-#score = model.evaluate(x_test, y_test, verbose=VERBOSE)
-#score = model.evaluate(x_train, y_train, verbose=VERBOSE)
-#print("\nscore:", score)
-#print("\nTest score:", score[0])
-#print('Test accuracy:', score[1])
-
-showHistory(history_object) 
-
-model.save('model.h5')
-
-visualize_model(model)
+	model.save('model.h5')
+	score = model.evaluate(x_test, y_test, verbose=VERBOSE)
+	print("Score:", score)
+	#print('Test accuracy:', score[1])
+	
+	display_graph(history_object) 
--- project-3/writeup.md
+++ project-3/writeup.md
@@ -1,11 +1,3 @@
-#**Behavioral Cloning** 
-
-##Writeup Template
-
-###You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
-
----
-
 **Behavioral Cloning Project**
 
 The goals / steps of this project are the following:
@@ -25,6 +17,8 @@ The goals / steps of this project are the following:
 [image5]: ./examples/placeholder_small.png "Recovery Image"
 [image6]: ./examples/placeholder_small.png "Normal Image"
 [image7]: ./examples/placeholder_small.png "Flipped Image"
+
+![](./images/graph.png)
 
 ## Rubric Points
 ###Here I will consider the [rubric points](https://review.udacity.com/#!/rubrics/432/view) individually and describe how I addressed each point in my implementation.  
@@ -58,9 +52,10 @@ My model consists of a convolution neural network with 3x3 filter sizes and dept
 
 The model includes RELU layers to introduce nonlinearity (code line 20), and the data is normalized in the model using a Keras lambda layer (code line 18). 
 
+
 ####2. Attempts to reduce overfitting in the model
 
-The model contains dropout layers in order to reduce overfitting (model.py lines 21). 
+The model contains dropout layers in order to reduce overfitting (model.py lines 111). 
 
 The model was trained and validated on different data sets to ensure that the model was not overfitting (code line 10-16). The model was tested by running it through the simulator and ensuring that the vehicle could stay on the track.
 
@@ -80,21 +75,39 @@ For details about how I created the training data, see the next section.
 
 The overall strategy for deriving a model architecture was to ...
 
-My first step was to use a convolution neural network model similar to the ... I thought this model might be appropriate because ...
+My first step was to use a convolution neural network model similar to the LeNet I thought this model might work because it is a fairly complex model.
 
 In order to gauge how well the model was working, I split my image and steering angle data into a training and validation set. I found that my first model had a low mean squared error on the training set but a high mean squared error on the validation set. This implied that the model was overfitting. 
 
 To combat the overfitting, I modified the model so that ...
 
-Then I ... 
+Then I switched my model from LeNet to the one introduced by NVIDEA.
 
-The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I ....
+The final step was to run the simulator to see how well the car was driving around track one. There were a few spots where the vehicle fell off the track... to improve the driving behavior in these cases, I augumented images using other cameras.
 
 At the end of the process, the vehicle is able to drive autonomously around the track without leaving the road.
 
 ####2. Final Model Architecture
 
-The final model architecture (model.py lines 18-24) consisted of a convolution neural network with the following layers and layer sizes ...
+The final model architecture (model.py: "build_model" function, lines 101-117) consisted of the following layers:
+
+| Layer         	|     Description		        	| 
+|:---------------------:|:---------------------------------------------:| 
+| Input         	| 160x320x3 RGB image   			| 
+| Lambda		|         					|
+| Cropping2D	      	| top: 70, bottom: 25, right: 0, left: 0	|
+| Convolution2D		| filter:24, kernal:5x5, strides: 2x2 activation: RELU	|
+| Convolution2D		| filter:36, kernal:5x5, strides: 2x2 activation: RELU	|
+| Convolution2D	      	| filter:48, kernal:5x5, strides: 2x2 activation: RELU	|
+| Convolution2D		| filter:64, kernal:3x3, activation: RELU	|
+| Convolution2D		| filter:64, kernal:3x3, activation: RELU	|
+| Dropout		| keep probability: 0.5        			|
+| Flatten		|        					|
+| Dence			| unit: 100	        			|
+| Dence			| unit: 50	        			|
+| Dence			| unit: 10	        			|
+| Dence			| unit: 1	        			|
+
 
 Here is a visualization of the architecture (note: visualizing the architecture is optional according to the project rubric)
 
@@ -123,7 +136,18 @@ Etc ....
 
 After the collection process, I had X number of data points. I then preprocessed this data by ...
 
-
 I finally randomly shuffled the data set and put Y% of the data into a validation set. 
 
 I used this training data for training the model. The validation set helped determine if the model was over or under fitting. The ideal number of epochs was Z as evidenced by ... I used an adam optimizer so that manually training the learning rate wasn't necessary.
+
+(carnd-term1) C:\Work\Udacity\Self-Driving Car Engineer\Project\CarND-Behavioral-Cloning-P3>python model.py
+Using TensorFlow backend.
+Train on 20569 samples, validate on 5143 samples
+Epoch 1/8
+20569/20569 [==============================] - 424s - loss: 0.0194 - val_loss: 0.0141
+Epoch 2/8
+20569/20569 [==============================] - 419s - loss: 0.0146 - val_loss: 0.0127
+Epoch 3/8
+20569/20569 [==============================] - 420s - loss: 0.0135 - val_loss: 0.0129
+6432/6432 [==============================] - 54s
+Score: 0.012020876879
